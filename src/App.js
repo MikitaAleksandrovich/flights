508a4a2b42559817588e520bc38cee83fb562658
Mikita Aleksandrovich <aleksandrovich.mikita@gmail.com>
2021-03-01 21:59:00 +0300

    Add filter by price

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 
 import { Flight } from './components';
 import Logo from './images/Logo.png'
@@ -7,15 +7,17 @@ import { flightTabIndexes } from './constants/constants';
 import styles from './App.module.css';
 
 const App = () => {
+    const [allTickets, setAllTickets] = useState([]);
     const [tickets, setTickets] = useState([]);
     const [error, setError] = useState(false);
-    const [activeTabIndex, setActiveTabIndex] = useState(0);
+    const [activeTabIndex, setActiveTabIndex] = useState(-1);
 
     useEffect(() => {
         (async function() {
             try {
                 const data = await fetchData();
                 setTickets(data.tickets);
+                setAllTickets(data.tickets);
             } catch (e) {
                 setError(true);
             }
@@ -24,7 +26,23 @@ const App = () => {
 
     const handleTabIndex = (index) => {
         setActiveTabIndex(index);
+        switch (index) {
+            case 0:
+                setTickets(tickets.filter((ticket) => ticket.price === Math.min(...tickets.map((ticket) => ticket.price))));
+                break;
+            case 1:
+                setTickets(allTickets);
+                break;
+            case 2:
+                setTickets(allTickets);
+                break;
+            default:
+                setTickets(allTickets);
+                break;
+        }
       };
+
+    console.log('tickets', tickets)
 
     return (
         <div className={styles.container}>
@@ -34,7 +52,7 @@ const App = () => {
                 <div className={`${styles.tab} ${activeTabIndex === index && styles.activeTab}`} onClick={() => handleTabIndex(index)}>{tab}</div>
             ))}
         </div>
-        {error ? <div className={styles.container}>Что-то пошло не так, пожалуйста, обновите страницу</div> :
+        {error ? <div className={styles.container}><div className={styles.errorMessage}>Что-то пошло не так, пожалуйста, обновите страницу</div></div> :
             tickets.map((ticket) => <Flight key={ticket.price} ticket={ticket}/>)
         }
     </div>
